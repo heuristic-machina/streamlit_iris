7b24caaec7eda7f480be07d51ad84117fa672577
heuristic_machina <machine_learner@yahoo.com>
2025-12-02 00:37:26 -0700

    plots added

--- pages/Exploratory.py
+++ pages/Exploratory.py
@@ -1,11 +1,13 @@
 import streamlit as st
-import pandas as pd
 import plotly.express as px
 from data_utils import load_iris
 
 
 df=load_iris()
+
 num_cols = df.select_dtypes(include='number').columns.tolist()
+obj_cols = df.select_dtypes(include='object').columns.tolist()
+
 
 st.title('Exploratory Data Analysis')
 st.subheader('Select the type of Visualization to Explore')
@@ -21,9 +23,24 @@ if 'Histograms' in eda_type:
         else:
             st.plotly_chart(px.histogram(df, x=h_selected_col, title=chart_title))
 
-# if 'Box Plots' in eda_type:
-#     st.subheader('Box Plots - Visualizing Numerical Distributions')
-#     b_selected_col = st.selectbox('Select a numerical column for the box plot:', num_cols)
-#     if b_selected_col:
-#         chart_title = f"Distribution of {b_selected_col.title().replace('_', ' ')}"
-#         st.plotly_chart(px.box(df, x='species', y=b_selected_col, title=chart_title, color='species'))
+if 'Box Plots' in eda_type:
+    st.subheader('Box Plots - Visualizing Numerical Distributions')
+    b_selected_col = st.selectbox('Select a numerical column for the box plot:', num_cols)
+    if b_selected_col:
+        chart_title = f"Distribution of {b_selected_col.title().replace('_', ' ')}"
+        st.plotly_chart(px.box(df, x='species', y=b_selected_col, title=chart_title, color='species'))
+
+if 'Scatter Plots' in eda_type:
+    st.subheader('Scatterplots - Visualizing Relationships')
+    selected_col_x = st.selectbox('Select x-axis variable:', num_cols)
+    selected_col_y = st.selectbox('Select y-axis variable:', num_cols)
+    if selected_col_x and selected_col_y:
+        chart_title = f"{selected_col_x.title().replace('_', ' ')} vs. {selected_col_y.title().replace('_', ' ')}"
+        st.plotly_chart(px.scatter(df, x=selected_col_x, y=selected_col_y, color='species', title=chart_title))
+
+if 'Count Plots' in eda_type:
+    st.subheader('Count Plots - Visualizing Categorical Distributions')
+    selected_col = st.selectbox('Select a categorical variable:', obj_cols)
+    if selected_col:
+        chart_title = f'Distribution of {selected_col.title()}'
+        st.plotly_chart(px.histogram(df, x=selected_col, color='species', title=chart_title))
